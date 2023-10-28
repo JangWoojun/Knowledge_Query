51abddd887434916dd2e65fe1c2b2135806eb9c0
JangWoojun <woojun0107@naver.com>
2023-10-29 07:43:03 +0900

    feat: Splash 자동 이동 추가

--- app/src/main/java/com/woojun/knowledge_query/SplashActivity.kt
+++ app/src/main/java/com/woojun/knowledge_query/SplashActivity.kt
@@ -1,11 +1,18 @@
 package com.woojun.knowledge_query
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Handler
 
 class SplashActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_splash)
+
+        Handler().postDelayed({
+            startActivity(Intent(this@SplashActivity, MainActivity::class.java)) // MainActivity 이동
+            finishAffinity()
+        }, 1500)
     }
 }
