6eb868051a6d4c395cccf2ee79d32302c4971347
JangWoojun <woojun0107@naver.com>
2023-10-30 04:12:08 +0900

    feat: MainActivity Navigation 세팅

--- app/src/main/java/com/woojun/knowledge_query/MainActivity.kt
+++ app/src/main/java/com/woojun/knowledge_query/MainActivity.kt
@@ -2,10 +2,33 @@ package com.woojun.knowledge_query
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.navigation.findNavController
+import com.woojun.knowledge_query.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.apply {
+            val navController = findNavController(R.id.nav_host_fragment)
+            bottomNavigation.setItemSelected(R.id.home)
+
+            bottomNavigation.setOnItemSelectedListener {
+                when (it) {
+                    R.id.home -> navController.navigate(R.id.home)
+                    R.id.myLibrary -> navController.navigate(R.id.myLibrary)
+                    R.id.bookmark -> navController.navigate(R.id.bookmark)
+                    R.id.setting -> navController.navigate(R.id.setting)
+                }
+            }
+
+            navController.addOnDestinationChangedListener { _, destination, _ ->
+                bottomNavigation.setItemSelected(destination.id)
+            }
+        }
+
     }
 }
