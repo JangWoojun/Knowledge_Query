45442731a81df22359926dcf64cd00497e526659
JangWoojun <woojun0107@naver.com>
2023-10-29 19:13:18 +0900

    feat: Preference 세팅

---
+++ app/src/main/java/com/woojun/knowledge_query/Preference.kt
@@ -0,0 +1,37 @@
+package com.woojun.knowledge_query
+
+import android.app.Application
+import android.content.Context
+import android.content.SharedPreferences
+
+class Preference : Application() {
+    companion object {
+        lateinit var prefs: PreferenceUtil
+    }
+
+    override fun onCreate() {
+        prefs = PreferenceUtil(applicationContext)
+        super.onCreate()
+    }
+}
+
+class PreferenceUtil(context: Context) {
+    private val prefs: SharedPreferences =
+        context.getSharedPreferences("default value", Context.MODE_PRIVATE)
+
+    fun getMode(): Boolean {
+        return prefs.getBoolean("mode", true)
+    }
+
+    fun setMode(mode: Boolean) {
+        prefs.edit().putBoolean("mode", mode).apply()
+    }
+
+    fun isFirst(): Boolean {
+        return prefs.getBoolean("isFirst", false)
+    }
+
+    fun notFirst() {
+        prefs.edit().putBoolean("isFirst", true).apply()
+    }
+}
