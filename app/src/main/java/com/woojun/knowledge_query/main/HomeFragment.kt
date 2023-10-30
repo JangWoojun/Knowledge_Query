368c35c37e3c3c93674880d529992c1e0f015114
JangWoojun <woojun0107@naver.com>
2023-10-31 07:42:55 +0900

    feat: BookRecyclerAdapter 연결

--- app/src/main/java/com/woojun/knowledge_query/main/HomeFragment.kt
+++ app/src/main/java/com/woojun/knowledge_query/main/HomeFragment.kt
@@ -5,12 +5,30 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.recyclerview.widget.GridLayoutManager
+import androidx.recyclerview.widget.LinearLayoutManager
+import com.woojun.knowledge_query.R
 import com.woojun.knowledge_query.databinding.FragmentHomeBinding
+import com.woojun.knowledge_query.util.BookInfo
+import com.woojun.knowledge_query.util.BookRecyclerAdapter
+import com.woojun.knowledge_query.util.BookType
+import com.woojun.knowledge_query.util.Space
+import com.woojun.knowledge_query.util.SpacesItemDecoration
 
 class HomeFragment : Fragment() {
 
     private var _binding: FragmentHomeBinding? = null
     private val binding get() = _binding!!
+
+    private val list = arrayListOf(
+        BookInfo("Catcher in the Rye", "J.D. Salinger", R.drawable.test_1, BookType.Category),
+        BookInfo("To Kill a Mockingbird", "Harper Lee", R.drawable.test_2, BookType.Category),
+        BookInfo("1984", "George Orwell", R.drawable.test_3, BookType.Category),
+        BookInfo("Brave New World", "Aldous Huxley", R.drawable.test_4, BookType.Category),
+        BookInfo("Moby Dick", "Herman Melville", R.drawable.test_5, BookType.Category),
+        BookInfo("War and Peace", "Leo Tolstoy", R.drawable.test_6, BookType.Category),
+        BookInfo("The Great Gatsby", "F. Scott Fitzgerald", R.drawable.test_7, BookType.Category),
+    )
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -28,6 +46,17 @@ class HomeFragment : Fragment() {
         super.onViewCreated(view, savedInstanceState)
 
         binding.apply {
+
+            categoryList.layoutManager = LinearLayoutManager(requireContext(), LinearLayoutManager.HORIZONTAL, false)
+            categoryList.adapter = BookRecyclerAdapter(list)
+
+            categoryList.addItemDecoration(SpacesItemDecoration(Space(0,  14, 0, 0, 16, 16), BookType.Category))
+
+
+            myList.layoutManager = GridLayoutManager(requireContext(), 2)
+            myList.adapter = BookRecyclerAdapter(list.subList(3, 7))
+
+            myList.addItemDecoration(SpacesItemDecoration(Space(0,  0, 0, 16, 16, 16), BookType.My))
 
         }
     }
