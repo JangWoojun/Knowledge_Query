eb76881b655af541e2ae500ba7af1aac8232df09
JangWoojun <woojun0107@naver.com>
2023-10-31 21:36:47 +0900

    fix: import 추가

--- app/src/main/java/com/woojun/knowledge_query/intro/IntroActivity.kt
+++ app/src/main/java/com/woojun/knowledge_query/intro/IntroActivity.kt
@@ -1,8 +1,11 @@
 package com.woojun.knowledge_query.intro
 
+import android.app.UiModeManager
+import android.content.Context
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.woojun.knowledge_query.databinding.ActivityIntroBinding
+import com.woojun.knowledge_query.util.Preference
 
 class IntroActivity : AppCompatActivity() {
     private lateinit var binding: ActivityIntroBinding
