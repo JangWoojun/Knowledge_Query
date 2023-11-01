51bd24256533f073e6eaf62b589bab6f3477572e
JangWoojun <woojun0107@naver.com>
2023-11-02 07:18:06 +0900

    feat: 데이터 바인딩 및 북마크 로직 추가

--- app/src/main/java/com/woojun/knowledge_query/main/BookInfoFragment.kt
+++ app/src/main/java/com/woojun/knowledge_query/main/BookInfoFragment.kt
@@ -5,7 +5,14 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import com.woojun.knowledge_query.R
 import com.woojun.knowledge_query.databinding.FragmentBookInfoBinding
+import com.woojun.knowledge_query.util.AppDatabase
+import com.woojun.knowledge_query.util.BookInfo
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.withContext
 
 class BookInfoFragment : Fragment() {
     private var _binding: FragmentBookInfoBinding? = null
@@ -28,6 +35,50 @@ class BookInfoFragment : Fragment() {
         super.onViewCreated(view, savedInstanceState)
 
         binding.apply {
+            val arguments = arguments
+            val item = arguments?.getParcelable<BookInfo>("book info")
+
+            image.setImageResource(item!!.image)
+            title.text = item.title
+            writer.text = item.writer
+            overviewText.text = item.overview
+            aboutAuthorText.text = item.aboutAuthor
+
+            CoroutineScope(Dispatchers.IO).launch {
+                val db = AppDatabase.getDatabase(requireContext())
+                val user = db?.userDao()!!.getUser()
+
+                withContext(Dispatchers.Main) {
+                    if (user.bookmark.contains(item)) {
+                        bookmark.setImageResource(R.drawable.fill_bookmark_icon)
+                    } else {
+                        bookmark.setImageResource(R.drawable.bookmark_icon)
+                    }
+                }
+
+            }
+
+            bookmarkButton.setOnClickListener {
+                CoroutineScope(Dispatchers.IO).launch {
+                    val db = AppDatabase.getDatabase(requireContext())
+                    val userDao = db?.userDao()
+                    val user = db?.userDao()!!.getUser()
+
+                    if (user.bookmark.contains(item)) {
+                        user.bookmark.removeAll { it == item }
+                        withContext(Dispatchers.Main){
+                            bookmark.setImageResource(R.drawable.bookmark_icon)
+                        }
+                    } else {
+                        user.bookmark.add(item)
+                        withContext(Dispatchers.Main){
+                            bookmark.setImageResource(R.drawable.fill_bookmark_icon)
+                        }
+                    }
+
+                    userDao!!.updateUser(user)
+                }
+            }
 
         }
     }
