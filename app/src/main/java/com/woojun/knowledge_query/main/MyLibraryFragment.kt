a828324a6567da34b107ed49b38ec5bbf1c29f48
JangWoojun <woojun0107@naver.com>
2023-11-05 11:53:09 +0900

    feat: bookNameInput 관련 세팅
    다른 뷰 클릭 시 다시 숨기기 및 bookNameLayout 클릭 시 bookNameInput에 포커스 추가 및 키보드 올리기

--- app/src/main/java/com/woojun/knowledge_query/main/MyLibraryFragment.kt
+++ app/src/main/java/com/woojun/knowledge_query/main/MyLibraryFragment.kt
@@ -1,15 +1,17 @@
 package com.woojun.knowledge_query.main
 
+import android.animation.Animator
+import android.animation.AnimatorListenerAdapter
 import android.animation.ObjectAnimator
 import android.content.Context
 import android.os.Bundle
-import android.view.ContextThemeWrapper
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.view.inputmethod.InputMethodManager
-import android.widget.PopupMenu
+import android.widget.TextView
+import androidx.core.content.ContextCompat
 import androidx.recyclerview.widget.GridLayoutManager
 import com.woojun.knowledge_query.R
 import com.woojun.knowledge_query.databinding.FragmentMyLibraryBinding
@@ -517,7 +519,21 @@ class MyLibraryFragment : Fragment() {
             bookList.adapter = adapter
             bookList.addItemDecoration(SpacesItemDecoration(Space(0,  0, 0, 16, 16, 16), BookType.My))
 
+            bookNameLayout.setOnClickListener {
+                bookNameInput.requestFocus()
+                showKeyboard(requireContext(), bookNameInput)
+            }
 
+            container.setOnTouchListener { it, _ ->
+                val imm = activity?.getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
+                imm.hideSoftInputFromWindow(it.windowToken, 0)
+
+                textView.visibility = View.VISIBLE
+                searchButton.visibility = View.VISIBLE
+                searchInputButton.visibility = View.GONE
+
+                true
+            }
         }
     }
 
