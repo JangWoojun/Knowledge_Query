3c2e2e9729bae4c038114a14c47d2cea3e32d839
JangWoojun <woojun0107@naver.com>
2023-11-07 03:28:45 +0900

    feat: ChatRecyclerAdapter RecyclerView에 연결

--- app/src/main/java/com/woojun/knowledge_query/main/QueryFragment.kt
+++ app/src/main/java/com/woojun/knowledge_query/main/QueryFragment.kt
@@ -5,12 +5,18 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.recyclerview.widget.LinearLayoutManager
 import com.woojun.knowledge_query.databinding.FragmentQueryBinding
+import com.woojun.knowledge_query.util.Chat
+import com.woojun.knowledge_query.util.ChatRecyclerAdapter
+import com.woojun.knowledge_query.util.ChatType
 
 class QueryFragment : Fragment() {
 
     private var _binding: FragmentQueryBinding? = null
     private val binding get() = _binding!!
+
+    private val list: MutableList<Chat> = mutableListOf(Chat(ChatType.Bot, "사용하시기 앞서 우선 쿼리 기능을 사용하실 글을 입력해주세요."), Chat(ChatType.User, "사용하시기 앞서 우선 쿼리 기능을 사용하실 글을 입력해주세요."), Chat(ChatType.User, "사용하시기 앞서 우선 쿼리 기능을 사용하실 글을 입력해주세요."), Chat(ChatType.Bot, "사용하시기 앞서 우선 쿼리 기능을 사용하실 글을 입력해주세요."), Chat(ChatType.Bot, "사용하시기 앞서 우선 쿼리 기능을 사용하실 글을 입력해주세요."))
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -28,7 +34,10 @@ class QueryFragment : Fragment() {
         super.onViewCreated(view, savedInstanceState)
 
         binding.apply {
+            chatList.layoutManager = LinearLayoutManager(requireContext(), LinearLayoutManager.VERTICAL, true)
+            chatList.adapter = ChatRecyclerAdapter(list)
 
+            chatList.smoothScrollToPosition(0)
         }
     }
 
