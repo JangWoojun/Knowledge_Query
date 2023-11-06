e49c9f542b037e6eedb006ba04cdbfcc443f9b26
JangWoojun <woojun0107@naver.com>
2023-11-07 07:57:35 +0900

    feat: Firebase Realtime Database 종속성 추가

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -67,6 +67,10 @@ dependencies {
     implementation("com.squareup.retrofit2:converter-gson:2.9.0")
 
     implementation("com.google.android.flexbox:flexbox:3.0.0")
-    implementation(platform("com.google.firebase:firebase-bom:32.5.0"))
+
     implementation("com.github.bumptech.glide:glide:4.16.0")
+
+    implementation(platform("com.google.firebase:firebase-bom:32.5.0"))
+    implementation("com.google.firebase:firebase-database-ktx")
+    implementation("com.google.firebase:firebase-analytics-ktx")
 }
