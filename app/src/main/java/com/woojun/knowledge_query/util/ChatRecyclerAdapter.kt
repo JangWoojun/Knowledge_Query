d9a663599c4dbe799233e88074795386752c6f4c
JangWoojun <woojun0107@naver.com>
2023-11-07 04:38:03 +0900

    feat: 유저와 봇 채팅을 구분하여 처리하는 방식 수정

--- app/src/main/java/com/woojun/knowledge_query/util/ChatRecyclerAdapter.kt
+++ app/src/main/java/com/woojun/knowledge_query/util/ChatRecyclerAdapter.kt
@@ -1,11 +1,9 @@
 package com.woojun.knowledge_query.util
 
-import android.graphics.Color
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
-import com.woojun.knowledge_query.R
 import com.woojun.knowledge_query.databinding.ChatItemBinding
 
 class ChatRecyclerAdapter(private val messages: List<Chat>): RecyclerView.Adapter<ChatRecyclerAdapter.ChatViewHolder>() {
@@ -30,15 +28,14 @@ class ChatRecyclerAdapter(private val messages: List<Chat>): RecyclerView.Adapte
         RecyclerView.ViewHolder(binding.root) {
         fun bind(chat: Chat) {
             binding.apply {
-                message.text = chat.message
-
                 if (chat.chatType == ChatType.User) {
-                    chatBackground.setBackgroundResource(R.drawable.my_chat_background)
-                    message.setTextColor(Color.parseColor("#ffffff"))
-                    imageView.visibility = View.INVISIBLE
+                    myMessage.text = chat.message
+                    myChatBackground.visibility = View.VISIBLE
+                    botChatBackground.visibility = View.GONE
                 } else {
-                    chatBackground.setBackgroundResource(R.drawable.bot_chat_background)
-                    message.setTextColor(Color.parseColor("#000000"))
+                    botMessage.text = chat.message
+                    botChatBackground.visibility = View.VISIBLE
+                    myChatBackground.visibility = View.GONE
                 }
             }
         }
