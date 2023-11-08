fb634a6dd2ecc7c92abc7308c50169acc45d096f
JangWoojun <woojun0107@naver.com>
2023-11-09 01:51:35 +0900

    feat: updateItem, addItem 추가

--- app/src/main/java/com/woojun/knowledge_query/util/ChatRecyclerAdapter.kt
+++ app/src/main/java/com/woojun/knowledge_query/util/ChatRecyclerAdapter.kt
@@ -6,7 +6,7 @@ import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
 import com.woojun.knowledge_query.databinding.ChatItemBinding
 
-class ChatRecyclerAdapter(private val messages: List<Chat>): RecyclerView.Adapter<ChatRecyclerAdapter.ChatViewHolder>() {
+class ChatRecyclerAdapter(private val messages: MutableList<Chat>): RecyclerView.Adapter<ChatRecyclerAdapter.ChatViewHolder>() {
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ChatViewHolder {
         val binding = ChatItemBinding.inflate(LayoutInflater.from(parent.context), parent, false)
@@ -21,8 +21,20 @@ class ChatRecyclerAdapter(private val messages: List<Chat>): RecyclerView.Adapte
     }
 
     override fun onBindViewHolder(holder: ChatViewHolder, position: Int) {
-        holder.bind(messages[position])
+        holder.bind(messages[position], )
     }
+
+    fun updateItem(position: Int, text: String, isEnd: Boolean) {
+        messages[position].message = text
+        messages[position].isAnswerEnd = isEnd
+        notifyItemChanged(position)
+    }
+
+    fun addItem(item: Chat) {
+        messages.add(item)
+        notifyItemInserted(messages.size - 1)
+    }
+
 
     class ChatViewHolder(private val binding: ChatItemBinding):
         RecyclerView.ViewHolder(binding.root) {
@@ -36,6 +48,12 @@ class ChatRecyclerAdapter(private val messages: List<Chat>): RecyclerView.Adapte
                     botMessage.text = chat.message
                     botChatBackground.visibility = View.VISIBLE
                     myChatBackground.visibility = View.GONE
+
+                    if (chat.isAnswerEnd) {
+                        loadingProgress.visibility = View.GONE
+                    } else {
+                        loadingProgress.visibility = View.VISIBLE
+                    }
                 }
             }
         }
