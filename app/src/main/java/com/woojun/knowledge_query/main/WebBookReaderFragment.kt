b8fcfbbc7932c734ea33db4d4ee6a4dd9cfe5cf7
JangWoojun <woojun0107@naver.com>
2023-11-10 06:56:16 +0900

    feat: Fragment 기본 설정

--- app/src/main/java/com/woojun/knowledge_query/main/WebBookReaderFragment.kt
+++ app/src/main/java/com/woojun/knowledge_query/main/WebBookReaderFragment.kt
@@ -6,55 +6,38 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import com.woojun.knowledge_query.R
+import com.woojun.knowledge_query.databinding.FragmentSearchBinding
+import com.woojun.knowledge_query.databinding.FragmentWebBookReaderBinding
 
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
-
-/**
- * A simple [Fragment] subclass.
- * Use the [WebBookReaderFragment.newInstance] factory method to
- * create an instance of this fragment.
- */
 class WebBookReaderFragment : Fragment() {
-    // TODO: Rename and change types of parameters
-    private var param1: String? = null
-    private var param2: String? = null
+
+    private var _binding: FragmentWebBookReaderBinding? = null
+    private val binding get() = _binding!!
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
-        }
+        (activity as MainActivity).hideBottomNavigation(true)
     }
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_web_book_reader, container, false)
+    ): View {
+        _binding = FragmentWebBookReaderBinding.inflate(inflater, container, false)
+        return binding.root
     }
 
-    companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment WebBookReaderFragment.
-         */
-        // TODO: Rename and change types and number of parameters
-        @JvmStatic
-        fun newInstance(param1: String, param2: String) =
-            WebBookReaderFragment().apply {
-                arguments = Bundle().apply {
-                    putString(ARG_PARAM1, param1)
-                    putString(ARG_PARAM2, param2)
-                }
-            }
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        binding.apply {
+
+        }
     }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        (activity as MainActivity).hideBottomNavigation(false)
+        _binding = null
+    }
+
 }
